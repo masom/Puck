564347f0cc522b332344f7afee573cccab97e720
Martin Samson <msamson@hcn-inc.com>
2011-11-25 11:35:24 -0500

    Moved the setup plugin to a separate file.

--- client/pixie.py
+++ client/pixie.py
@@ -1,6 +1,4 @@
-import cherrypy, os, sys, time, pickle, threading, Queue as queue
-
-from cherrypy.process import wspbus, plugins
+import cherrypy, os, sys, time
 
 from mako.template import Template
 from mako.lookup import TemplateLookup
@@ -8,6 +6,7 @@ from mako.lookup import TemplateLookup
 from lib.vm import VM
 from lib.puck import Puck
 from lib.jails import *
+from lib.setup_plugin import SetupPlugin
 
 class Controller(object):
     def _cp_on_error(self):
@@ -174,109 +173,6 @@ class Root(Controller):
             VM=self._vm
         )
         return self.render('/index.html', **env)
-
-class SetupPlugin(plugins.SimplePlugin):
-    class SetupTask(object):
-        _nameCounter = 0
-
-        def __init__(self, name = 'DefaultTask'):
-            self.name = "%s-%s" % (name, self.__class__._nameCounter)
-            self.__class__._nameCounter += 1
-        def run(self):
-            raise RuntimeError("`run` must be defined.")
-
-    class EZJailTask(SetupPlugin.SetupTask):
-        def run(self):
-            pass
-
-    class SetupWorkerThread(threading.Thread):
-        """Thread class with a stop() method. The thread itself has to check
-        regularly for the stopped() condition."""
-    
-        def __init__(self, bus=None, queue=None):
-            super(self.__class__, self).__init__()
-            self._stop = threading.Event()
-            self._queue = queue
-            self._bus = bus
-
-        def stop(self):
-            self._stop.set()
-    
-        def stopped(self):
-            return self._stop.isSet()
-
-        def _getTask(self, blocking = True, timeout = 10):
-            try:
-                task = self._queue.get(blocking, timeout)
-            except queue.Empty as e:
-                return None
-            return task
- 
-        def run(self):
-            self._bus.log("%s started." % self.__class__)
-            task = self._getTask()
-            while task:
-                self._bus.log("SetupWorkerThread received task: %s" % task)
-                time.sleep(1) 
-                task = self._getTask()
-
-    def __init__(self, bus, freq=30.0):
-        plugins.SimplePlugin.__init__(self, bus)
-        self.freq = freq
-        self._queue = queue.Queue()
-        self.worker = self.__class__.SetupWorkerThread( bus=bus, queue = self._queue)
-
-    def start(self):
-        self.bus.log('Starting up setup tasks')
-        self.bus.subscribe('setup', self.switch)
-
-    start.priority = 70
-
-    def stop(self):
-        self.bus.log('Stopping down setup task.')
-        self._setup_stop();
-
-    def switch(self, *args, **kwargs):
-        self.bus.log("Switch called. Linking call.")
-        if not 'action' in kwargs:
-            return
-
-        def default(**kwargs):
-            return
-
-        {
-         'start': self._setup_start,
-         'stop': self._setup_stop,
-         'status': self._setup_status
-        }.get(kwargs['action'], default)()
-
-    def _setup_stop(self, **kwargs):
-        self.bus.log("Stop called. Giving back time.")
-        if self.worker.isAlive():
-            self.worker.stop()
-
-    def _setup_start(self, **kwargs):
-        
-        if not self.worker.isAlive():
-            self.bus.log("Start called. Starting worker.")
-            self.worker.start()
-
-        self.bus.log("Building task list")
-        tasks = [
-            SetupPlugin.SetupTask('ezjail-setup'),
-            SetupPlugin.SetupTask('fetch'),
-            SetupPlugin.SetupTask('install'),
-            SetupPlugin.SetupTask('configure'),
-            SetupPlugin.SetupTask('start'),
-            SetupPlugin.SetupTask('status')
-        ]
-        self.bus.log("Publishing tasks")
-        for task in tasks:
-            self.bus.log("\t Publishing: %s" % task.name)
-            self._queue.put(task)
-
-    def _setup_status(self, **kwargs):
-        self.bus.log('Status called. Wants its time back.')
 
 puck = Puck()
 vm = puck.getVM()
